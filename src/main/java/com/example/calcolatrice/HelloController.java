9eff0560e5af74078a36aea56933d81eb00a2dc5
ila2312 <ilariadance98@live.it>
2022-10-02 19:20:28 +0200

    first commit
    hello

---
+++ src/main/java/com/example/calcolatrice/HelloController.java
@@ -0,0 +1,50 @@
+package com.example.calcolatrice;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label _screen;
+
+    @FXML
+    private Button _b1;
+
+    @FXML
+    private Button _b2;
+
+    @FXML
+    private Button _b3;
+
+    @FXML
+    private Button _b4;
+
+    @FXML
+    private Button _b5;
+
+    @FXML
+    private Button _b6;
+
+    @FXML
+    private Button _b7;
+
+    @FXML
+    private Button _b8;
+
+    @FXML
+    private Button _b9;
+
+    @FXML
+    private Button _b0;
+
+    @FXML
+    private Button _plus;
+
+    @FXML
+    private Button _min;
+
+    @FXML
+    private Button _equal;
+
+}
